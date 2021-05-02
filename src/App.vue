82801012626699c4ae58875f7b8e30979a5df1e0
Odumz <odumz20@gmail.com>
2021-05-02 13:10:52 +0100

    dark theme and other themes created, button component started

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-<h1>Haulage App</h1>
+<div :class="[darkClass, theme]">
+  <h1>Haulage App</h1>
+  <Button name="simple">Text content</Button>
+</div>
+
 </template>
 
 <script>
+import Button from './components/buttons/button.vue'
 
 export default {
   name: 'App',
   components: {
-  }
+    Button,
+  },
+  mounted() {
+    this.getClass()
+  },
+  data() {
+    return {
+      darkMode: false,
+      darkClass: '',
+      theme: ''
+    }
+  },
+  methods: {
+    getClass() {
+      this.darkClass = this.darkMode ? 'dark' : ''
+      this.theme = 'theme-neon'
+    }
+  },
 }
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)',
+        }
+      },
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)',
+        }
+      },
+      gradientColorStops: {
+        skin: {
+          hue: 'var(--color-fill)'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
