dd1d01c292a8b17d1883f8650e5879f247d5d5f6
Odumz <odumz20@gmail.com>
2021-05-02 09:53:58 +0100

    component init

---
+++ src/components/cards/card.vue
@@ -0,0 +1,21 @@
+<template>
+    <cardheader />
+    <cardfooter />
+</template>
+
+<script>
+import cardheader from './card-header'
+import cardfooter from './card-footer'
+
+export default {
+    components: {
+        cardheader,
+        cardfooter
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
