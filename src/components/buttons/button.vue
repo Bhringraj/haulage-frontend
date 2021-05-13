fcdf887ca270ef48fd98681aefd8788e73301946
Odumz <odumz20@gmail.com>
2021-05-13 12:39:14 +0100

    button components updated

--- src/components/buttons/button.vue
+++ src/components/buttons/button.vue
@@ -6,10 +6,13 @@
         <button v-else-if="name == 'pill-inverted'" class = "text-skin-inverted dark:bg-gray-800 bg-skin-button-accent dark:hover:bg-skin-button-muted hover:bg-skin-button-muted dark:hover:text-gray-800 hover:text-skin-base py-3 px-4 font-bold rounded-full">
             <slot></slot>
         </button>
-        <button v-else-if="name == 'outline'" class = "bg-transparent hover:bg-skin-button-muted text-skin-inverted  dark:hover:text-gray-800 hover:text-skin-base py-3 px-4 font-semibold border border-skin-muted hover:border-transparent rounded">
+        <button v-else-if="name == 'outline'" class = "bg-transparent text-skin-inverted py-3 px-4 font-semibold border-2 hover:border-opacity-80 border-skin-muted hover:shadow-md rounded">
             <slot></slot>
         </button>
-        <div v-else-if="name == 'groups-inverted'" class = "inline-flex">
+        <button v-else-if="name == 'pill-outline'" class = "bg-transparent text-skin-inverted py-3 px-4 font-semibold border-2 hover:border-opacity-80 border-skin-muted hover:shadow-md rounded-full">
+            <slot></slot>
+        </button>
+        <div v-else-if="name == 'group-inverted'" class = "inline-flex">
             <button class="text-skin-inverted dark:bg-gray-800 dark:hover:text-gray-800 bg-skin-button-accent dark:hover:bg-skin-button-muted hover:bg-skin-button-muted hover:text-skin-base py-3 px-4 font-bold rounded-l">
                 <slot name="btn1"></slot> 
             </button>
@@ -23,11 +26,27 @@
         <button v-else-if="name == 'pill'" class = "text-skin-base dark:text-gray-800 bg-skin-fill hover:bg-skin-button-muted py-3 px-4 font-bold rounded-full">
             <slot></slot>
         </button>
-        <div v-else-if="name == 'groups'" class = "inline-flex">
+        <div v-else-if="name == 'group'" class = "inline-flex">
             <button class="text-skin-base dark:text-gray-800 bg-skin-fill hover:bg-skin-button-muted py-3 px-4 font-bold rounded-l">
                 <slot name="btn1"></slot> 
             </button>
             <button class="text-skin-base dark:text-gray-800 bg-skin-fill hover:bg-skin-button-muted py-3 px-4 font-bold rounded-r">
+                <slot name="btn2"></slot> 
+            </button>
+        </div>
+        <div v-else-if="name == 'group-outline'" class = "inline-flex">
+            <button class="text-skin-inverted bg-transparent border-2 border-skin-muted hover:shadow-md hover:opacity-80 py-3 px-4 font-bold rounded-l">
+                <slot name="btn1"></slot> 
+            </button>
+            <button class="text-skin-inverted bg-transparent border-2 border-skin-muted hover:shadow-md hover:opacity-80 py-3 px-4 font-bold rounded-r">
+                <slot name="btn2"></slot> 
+            </button>
+        </div>
+        <div v-else-if="name == 'group-outline-pill'" class = "inline-flex">
+            <button class="text-skin-inverted bg-transparent border-2 border-skin-muted hover:shadow-md hover:opacity-80 py-3 px-4 font-bold rounded-l-full">
+                <slot name="btn1"></slot> 
+            </button>
+            <button class="text-skin-inverted bg-transparent border-2 border-skin-muted hover:shadow-md hover:opacity-80 py-3 px-4 font-bold rounded-r-full">
                 <slot name="btn2"></slot> 
             </button>
         </div>
--- src/views/temp.vue
+++ src/views/temp.vue
@@ -1,17 +1,23 @@
 <template>
 <div :class="[darkClass, theme]">
   <Header />
-  <alerts class="mb-3" v-show="show" :class="alertType" :name="alertName">
+  <alerts class="info mb-3" v-show="show" name="modern-badge">
     <!-- <template v-slot:icon><svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg></template> -->
     <template v-slot:title>Holy smokes! </template>
     <template v-slot:text>Something seriously bad happened. </template>
   </alerts>
   <!-- <h1>Haulage App</h1> -->
   <!-- <maincard /> -->
-  <Button :name="button" class="mb-2">Text content</Button>
-  <Button name="pill-inverted" class="my-2">Text content</Button>
-  <Button name="outline">Text content</Button>
-  <Button class="mt-3" name="groups-inverted"><template v-slot:btn1>Light </template><template v-slot:btn2> Dark</template></Button>
+  <Button name="simple" class="my-2">simple filled</Button>
+  <Button name="pill" class="my-2">filled pill</Button>
+  <Button class="my-3" name="group"><template v-slot:btn1>filled </template><template v-slot:btn2> group</template></Button>
+  <Button name="simple-inverted" class="mb-2">simple inverted</Button>
+  <Button name="pill-inverted" class="my-2">pill inverted</Button>
+  <Button class="my-3" name="group-inverted"><template v-slot:btn1>group </template><template v-slot:btn2> inverted</template></Button>
+  <Button name="pill-outline" class="my-2">pill outline</Button>
+  <Button name="outline" class="my-2">simple outline</Button>
+  <Button class="my-3" name="group-outline"><template v-slot:btn1>group </template><template v-slot:btn2> outline</template></Button>
+  <Button class="my-3" name="group-outline-pill"><template v-slot:btn1>Light </template><template v-slot:btn2> Dark</template></Button>
   <temp />
   <!-- <temp class="theme-neon" /> -->
   <themeswitch name="dark" :mode=mode @toggle="toggle" ><template v-slot:before>Light </template><template v-slot:after> Dark</template></themeswitch>
@@ -39,9 +45,6 @@ export default {
     Header,
     // maincard
   },
-  mounted() {
-    this.getClass()
-  },
   created() {
     window.addEventListener('keyup', this.keyPress)
   },
@@ -50,9 +53,6 @@ export default {
       darkClass: '',
       theme: '',
       mode: 'light',
-      alertName: '',
-      alertType: '',
-      button: '',
       show: true
     }
   },
@@ -61,11 +61,6 @@ export default {
       if (e.key === 'd') {
         this.toggle()
       }
-    },
-    getClass() {
-      this.alertName = 'solid'
-      this.button = 'simple-inverted'
-      this.alertType = 'info'
     },
     toggle () {
       if (this.mode === "dark") {
